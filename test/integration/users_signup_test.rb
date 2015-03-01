cd88996bc94f19152e471119c2e85d95599c3db9
Justin Forcier <zertukis@gmail.com>
2015-03-01 08:59:13 +0000

    finish login/out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,14 +12,4 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
-  test "valid signup information" do
-    get signup_path
-    assert_difference 'User.count' do
-      post_via_redirect users_path, user: { name: "",
-                                            email: "user@invalid",
-                                            password: "foo",
-                                            password_confirmation: "bar" }
-    end
-    assert_template 'users/show'
-  end
 end
